feb99b470567904bc4984fd7e1180b21df157092
AqibTuri <aqib.turi.work@gmail.com>
2025-10-19 07:19:59 +0500

    Feat: staircase hackerrank problem with 3 best solutions and time complexity for each

---
+++ Stair_Case_Problem_1.js
@@ -0,0 +1,65 @@
+// Print this pattern 
+// Like if n is 4 then it should look like
+/*
+       #
+      ##
+     ###
+    ####
+*/  
+
+// Solution 1
+
+function printSpace (n) {
+    let space = '';
+     for(let i =0 ; i < n ; i ++){
+        space +=  ` `
+     }
+     return space
+}
+
+function printHash (n) {
+    let hash = '';
+     for(let i =0 ; i < n ; i ++){
+        hash += `#`
+     }
+     return hash
+}
+
+function staircase_way_1(n) {
+    // Write your code here
+    let lineNumber = 1;
+    for (let i=n; i >0; i--){
+        console.log(printSpace(n -lineNumber) + printHash(lineNumber));
+        lineNumber++;
+    }
+}
+staircase_way_1(4)
+// Time Complexity: O(N^2) 
+
+/*
+ More Better solution 
+*/
+
+// Solution 2  
+// Time Complexity: O(N^2) 
+function staircase_way_2(n) {
+  for (let i = 1; i <= n; i++) {
+    console.log(' '.repeat(n - i) + '#'.repeat(i));
+  }
+}
+
+staircase_way_2(10);
+
+
+// Solution 3 Most fit soltuion 
+// Time Complexity: O(N * m) -> O(N);  
+function staircase_way_3(n) {
+  let line = ' '.repeat(n - 1) + '#';
+  console.log(line);
+  for (let i = 1; i < n; i++) {
+    line = line.substring(1) + '#';
+    console.log(line);
+  }
+}
+
+staircase_way_3(5)
